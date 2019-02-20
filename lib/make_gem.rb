46cc8344effdff099a91fbfd24c79ad72aa14853
PeterCamilleri <peter.c.camilleri@gmail.com>
2019-02-20 14:43:56 -0500

    Began last manual conversion.

--- lib/make_gem.rb
+++ lib/make_gem.rb
@@ -1,6 +1,65 @@
-require "make_gem/version"
+# Build a gem framework without having to hack a ton of stuff.
 
-module MakeGem
-  class Error < StandardError; end
-  # Your code goes here...
+require_relative 'make_gem/version'
+
+puts "make gem utility version: #{::MakeGem::VERSION}"
+
+# Check parameter and extract data.
+if ARGV.empty?
+  puts "Usage: make_gem your_gem_name", ""
+  exit
 end
+
+gem_name = ARGV[0]
+
+if gem_name !~ /\A[a-z][a-z0-9_]*\z/
+  puts "Invalid gem name: #{gem_name.inspect}", ""
+  exit
+end
+
+class_name = gem_name.split("_").map(&:capitalize).join
+user_name  = (%x{git config --global user.name}).chomp
+user_email = (%x{git config --global user.email}).chomp
+here       = Dir.pwd + "/"
+there      = here + gem_name + "/"
+
+
+evaluator = binding
+
+puts "Creating gem = #{gem_name}",
+     "Module name  = #{class_name}",
+     "User name    = #{user_name}",
+     "User email   = #{user_email}",
+     "Current      = #{here}",
+     "Target       = #{there}",
+     ""
+
+# Verify that we may proceed.
+print "Proceed? (y/n): "
+exit unless STDIN.gets[0] == "y"
+
+# Process the request.
+
+puts "0. Establishing starting point."
+%x{bundle gem #{gem_name}}
+Dir.chdir(gem_name)
+%x{git commit -m "Initial commit from bundler."}
+
+puts "1. Removing extraneous files."
+%x{git rm .travis.yml}
+%x{git rm bin/console}
+%x{git rm bin/setup}
+%x{git commit -m "Fix phase 1"}
+
+puts "2. Fix up the rake file."
+%x{git mv Rakefile rakefile.rb\n}
+%x{git commit -m "Fix phase 2"}
+
+puts "3. Fix up the test files."
+%x{git rm test/test_helper.rb}
+%x{git commit -m "Fix phase 3"}
+
+# Bundler::VERSION.partition(/\d\.\d+/)
+# "fooo.md.erb".sub(/\.erb/, "")
+
+puts "** Done. **"
--- lib/make_gem/version.rb
+++ lib/make_gem/version.rb
@@ -1,3 +1,5 @@
 module MakeGem
-  VERSION = "0.1.0"
+  VERSION = "0.1.0".freeze
+
+  DESCRIPTION = "make_gem: Create a gem frame work ready to begin useful work.".freeze
 end
