4611654e1ddfe22cf1d6364883db72e3c822c40f
PeterCamilleri <peter.c.camilleri@gmail.com>
2019-02-20 14:24:03 -0500

    Initial commit from bundler.

---
+++ lib/make_gem.rb
@@ -0,0 +1,6 @@
+require "make_gem/version"
+
+module MakeGem
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/make_gem_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class MakeGemTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::MakeGem::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
