74c7fa9d3bbf08d3beb27835454543d0a1faf779
PeterCamilleri <peter.c.camilleri@gmail.com>
2019-02-20 19:10:41 -0500

    Fix the gem's own test file.

--- test/make_gem_test.rb
+++ test/make_gem_test.rb
@@ -1,13 +1,22 @@
-require_relative '../lib/make_gemerb'
+require_relative '../lib/make_gem'
 require          'minitest/autorun'
 require          'minitest_visible'
 
-module MakeGemTest < Minitest::Test
+class MakeGemTest < Minitest::Test
   def test_that_it_has_a_version_number
-    refute_nil ::MakeGem::VERSION
+    refute_nil(::MakeGem::VERSION)
+    assert(::MakeGem::VERSION.frozen?)
+    assert(::MakeGem::VERSION.is_a?(String))
+    assert(/\A\d+\.\d+\.\d+/ =~ ::MakeGem::VERSION)
+  end
+
+  def test_that_it_has_a_description
+    refute_nil(::MakeGem::DESCRIPTION)
+    assert(::MakeGem::DESCRIPTION.frozen?)
+    assert(::MakeGem::DESCRIPTION.is_a?(String))
   end
 
   def test_it_does_something_useful
-    assert false
+    assert(false)
   end
 end
